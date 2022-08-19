d7a3bb97a84c546db4c25230210471dd1c1b476f
Vladimir <duenasvladimir@gmail.com>
2022-08-18 23:15:02 -0600

    Added new test suites

--- spec/grid_spec.rb
+++ spec/grid_spec.rb
@@ -3,102 +3,96 @@
 
 require './lib/grid'
 
-class DummyGrid
+class TestGrid
   include Grid
 end
 
 RSpec.describe Grid do
   describe '#find_in_grid' do
-    it 'Returns the position of a value in a bidimensional array' do
+    it 'returns the position of a value in a bidimensional array' do
       arr = [%w[1 2 3], %w[4 5 6], %w[7 8 9]]
-      grid = DummyGrid.new
+      grid = TestGrid.new
       expect(grid.find_in_grid('5', arr)).to eql([1, 1])
     end
 
-    it 'Returns the position of a value in a bidimensional array' do
+    it 'returns the position of a value in a bidimensional array' do
       arr = [%w[1 2 3], %w[4 5 6], %w[7 8 9]]
-      grid = DummyGrid.new
+      grid = TestGrid.new
       expect(grid.find_in_grid('9', arr)).to eql([2, 2])
     end
 
-    it 'Returns [nil, nil] if it doesn\'t find the element' do
+    it 'returns [nil, nil] if it doesn\'t find the element' do
       arr = [%w[1 2 3], %w[4 5 6], %w[7 8 9]]
-      grid = DummyGrid.new
+      grid = TestGrid.new
       expect(grid.find_in_grid('10', arr)).to eql([nil, nil])
     end
 
-    it 'Returns [nil, nil] if given an empty array' do
+    it 'returns [nil, nil] if given an empty array' do
       arr = [[]]
-      grid = DummyGrid.new
+      grid = TestGrid.new
       expect(grid.find_in_grid('1', arr)).to eql([nil, nil])
     end
   end
-end
 
-RSpec.describe Grid do
   describe '#find_horizontal_pattern' do
-    it 'Returns nil when it doesn\'t find a pattern' do
+    it 'returns nil when it doesn\'t find a pattern' do
       arr = [%w[1 2 3]]
-      grid = DummyGrid.new
+      grid = TestGrid.new
       expect(grid.find_horizontal_pattern(arr)).to be_nil
     end
 
-    it 'Returns nil given an empty array' do
+    it 'returns nil given an empty array' do
       arr = [[]]
-      grid = DummyGrid.new
+      grid = TestGrid.new
       expect(grid.find_horizontal_pattern(arr)).to be_nil
     end
 
-    it 'Returns the repeating value when it finds a pattern' do
+    it 'returns the repeating value when it finds a pattern' do
       arr = [%w[1 2 3], %w[4 4 4]]
-      grid = DummyGrid.new
+      grid = TestGrid.new
       expect(grid.find_horizontal_pattern(arr)).to eql('4')
     end
 
-    it 'Returns the repeating value when it finds a pattern' do
+    it 'returns the repeating value when it finds a pattern' do
       arr = [%w[1 2 3], %w[x x x]]
-      grid = DummyGrid.new
+      grid = TestGrid.new
       expect(grid.find_horizontal_pattern(arr)).to eql('x')
     end
   end
-end
 
-RSpec.describe Grid do
   describe '#transpose' do
-    it 'Returns the transpose of an array' do
+    it 'returns the transpose of an array' do
       arr = [%w[1 2 3], %w[4 5 6], %w[7 8 9]]
       expected_result = [%w[1 4 7], %w[2 5 8], %w[3 6 9]]
-      grid = DummyGrid.new
+      grid = TestGrid.new
       expect(grid.transpose(arr)).to eql(expected_result)
     end
 
-    it 'Returns the transpose of an array' do
+    it 'returns the transpose of an array' do
       arr = [%w[9 8 7], %w[6 5 4], %w[3 2 1]]
       expected_result = [%w[9 6 3], %w[8 5 2], %w[7 4 1]]
-      grid = DummyGrid.new
+      grid = TestGrid.new
       expect(grid.transpose(arr)).to eql(expected_result)
     end
 
-    it 'Returns the transpose of an array of emtpy arrays' do
+    it 'returns the transpose of an array of emtpy arrays' do
       arr = [[], [], []]
-      grid = DummyGrid.new
+      grid = TestGrid.new
       expect(grid.transpose(arr)).to eql(arr)
     end
 
-    it 'Returns the array when given an unidimensional array' do
+    it 'returns the array when given an unidimensional array' do
       arr = []
-      grid = DummyGrid.new
+      grid = TestGrid.new
       expect(grid.transpose(arr)).to eql(arr)
     end
   end
-end
 
-RSpec.describe Grid do
   describe '#find_diagonals' do
-    it 'Returns both diagonals' do
+    it 'returns both diagonals' do
       arr = [%w[1 2 3], %w[4 5 6], %w[7 8 9]]
       expected_result = [%w[1 5 9], %w[3 5 7]]
-      grid = DummyGrid.new
+      grid = TestGrid.new
       expect(grid.find_diagonals(arr)).to eql(expected_result)
     end
   end
--- spec/tictactoe_spec.rb
+++ spec/tictactoe_spec.rb
@@ -5,18 +5,88 @@ require './lib/tictactoe'
 
 RSpec.describe TicTacToe do
   describe '#show_grid' do
-    it '`puts` the initial grid formatted' do
+    it 'returns the initial grid formatted' do
       tictactoe = TicTacToe.new
       formatted_grid = "1 |2 |3\n-------\n4 |5 |6\n-------\n7 |8 |9\n\n"
       expect(tictactoe.show_grid).to eql(formatted_grid)
     end
 
-    it '`puts` current grid formatted' do
+    it 'returns current grid formatted' do
       tictactoe = TicTacToe.new
       tictactoe.place_simbol('5')
       tictactoe.place_simbol('2')
       formatted_grid = "1 |o |3\n-------\n4 |x |6\n-------\n7 |8 |9\n\n"
       expect(tictactoe.show_grid).to eql(formatted_grid)
+    end
+  end
+
+  describe '#ended' do
+    it 'returns false when game starts' do
+      tictactoe = TicTacToe.new
+      expect(tictactoe.ended).to eql(false)
+    end
+
+    it 'returns true when a player wins in a row' do
+      tictactoe = TicTacToe.new
+      tictactoe.place_simbol('1')
+      tictactoe.place_simbol('4')
+      tictactoe.place_simbol('2')
+      tictactoe.place_simbol('5')
+      tictactoe.place_simbol('3')
+
+      expect(tictactoe.ended).to eql(true)
+    end
+
+    it 'returns true when a player wins in a column' do
+      tictactoe = TicTacToe.new
+      tictactoe.place_simbol('1')
+      tictactoe.place_simbol('2')
+      tictactoe.place_simbol('4')
+      tictactoe.place_simbol('5')
+      tictactoe.place_simbol('7')
+
+      expect(tictactoe.ended).to eql(true)
+    end
+
+    it 'returns true when a player wins in a diagonal' do
+      tictactoe = TicTacToe.new
+      tictactoe.place_simbol('1')
+      tictactoe.place_simbol('2')
+      tictactoe.place_simbol('5')
+      tictactoe.place_simbol('6')
+      tictactoe.place_simbol('9')
+
+      expect(tictactoe.ended).to eql(true)
+    end
+
+    it 'returns true if it\'s a tie' do
+      tictactoe = TicTacToe.new
+      tictactoe.place_simbol('1')
+      tictactoe.place_simbol('2')
+      tictactoe.place_simbol('3')
+      tictactoe.place_simbol('5')
+      tictactoe.place_simbol('4')
+      tictactoe.place_simbol('6')
+      tictactoe.place_simbol('8')
+      tictactoe.place_simbol('7')
+      tictactoe.place_simbol('9')
+
+      expect(tictactoe.ended).to eql(true)
+    end
+  end
+
+  describe '#winner' do
+    it 'returns the correct winner' do
+      tictactoe = TicTacToe.new
+      tictactoe.place_simbol('1')
+      tictactoe.place_simbol('4')
+      tictactoe.place_simbol('2')
+      tictactoe.place_simbol('5')
+      tictactoe.place_simbol('3')
+
+      # since the last place simbol call updates the player
+      # the winner cannot be the current player
+      expect(tictactoe.winner).to_not eql(tictactoe.player)
     end
   end
 end
