6a94c1120dda360ef6d8ae4908eb90e526475457
Vladimir <duenasvladimir@gmail.com>
2022-08-18 22:30:52 -0600

    Added test suites to the tictactoe class

--- spec/tictactoe_spec.rb
+++ spec/tictactoe_spec.rb
@@ -1,11 +1,22 @@
-# frozen_literal_string: false
 # spec/tictactoe_spec.rb
+# frozen_string_literal: false
 
 require './lib/tictactoe'
 
 RSpec.describe TicTacToe do
-  describe '#tie?' do
-    it 'Returns true when there\'s' do
+  describe '#show_grid' do
+    it '`puts` the initial grid formatted' do
+      tictactoe = TicTacToe.new
+      formatted_grid = "1 |2 |3\n-------\n4 |5 |6\n-------\n7 |8 |9\n\n"
+      expect(tictactoe.show_grid).to eql(formatted_grid)
+    end
+
+    it '`puts` current grid formatted' do
+      tictactoe = TicTacToe.new
+      tictactoe.place_simbol('5')
+      tictactoe.place_simbol('2')
+      formatted_grid = "1 |o |3\n-------\n4 |x |6\n-------\n7 |8 |9\n\n"
+      expect(tictactoe.show_grid).to eql(formatted_grid)
     end
   end
 end
